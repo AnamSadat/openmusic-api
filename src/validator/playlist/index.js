05df679934ba598c2ab3f3e993fd82283d9d991e
AnamSadat <anamsadat3@gmail.com>
2025-09-10 19:56:30 +0700

    feat(shcema): validation for add song with playlist

--- src/validator/playlist/index.js
+++ src/validator/playlist/index.js
@@ -1,12 +1,15 @@
 import InvariantError from '../../exceptions/InvariantError.js';
-import PlaylistSchema from './schema.js';
+import { PlaylistSchema, SongSchema } from './schema.js';
 
 const PlaylistValidator = {
   validatePlaylistPayload: (payload) => {
     const validationResult = PlaylistSchema.validate(payload);
-    if (validationResult.error) {
-      throw new InvariantError(validationResult.error.message);
-    }
+    if (validationResult.error) throw new InvariantError(validationResult.error.message);
+  },
+
+  validateSongPlaylistPayload: (payload) => {
+    const validationResult = SongSchema.validate(payload);
+    if (validationResult.error) throw new InvariantError(validationResult.error.message);
   },
 };
 
