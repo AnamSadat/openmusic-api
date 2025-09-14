6a0adaffb56f351b48219b887098bc656460d618
AnamSadat <anamsadat3@gmail.com>
2025-09-14 21:11:45 +0700

    feat: implement config environment for reusable

---
+++ src/utils/config.js
@@ -0,0 +1,34 @@
+import dotenv from 'dotenv';
+import EnvironmentValidation from '../validator/env/index.js';
+
+dotenv.config();
+
+const validation = EnvironmentValidation.validateEnv();
+
+const config = {
+  app: {
+    node_env: validation.NODE_ENV,
+    host: validation.HOST,
+    port: validation.PORT,
+  },
+  db: {
+    user: validation.PGUSER,
+    password: validation.PGPASSWORD,
+    database: validation.PGDATABASE,
+    host: validation.PGHOST,
+    port: validation.PGPORT,
+  },
+  auth: {
+    accessTokenKey: validation.ACCESS_TOKEN_KEY,
+    refreshTokenKey: validation.REFRESH_TOKEN_KEY,
+    accessTokenAge: validation.ACCESS_TOKEN_AGE,
+  },
+  rabbitMq: {
+    server: validation.RABBITMQ_SERVER,
+  },
+  redis: {
+    host: validation.REDIS_SERVER,
+  },
+};
+
+export default config;
