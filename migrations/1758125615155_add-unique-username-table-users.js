a7f02234230f35daeb13420cf48653e21de5be9f
AnamSadat <anamsadat3@gmail.com>
2025-09-18 02:08:49 +0700

    feat(postgres): migrate unique username

---
+++ migrations/1758125615155_add-unique-username-table-users.js
@@ -0,0 +1,22 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.addConstraint('users', 'unique_username', 'UNIQUE(username)');
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropConstraint('users', 'unique_username');
+};
